4bce125bcfdb6eb5e64814321c2943d65d6f01b4
MsRioux <jrioux@mvusd.net>
2020-03-09 13:16:27 -0700

    Initial commit

---
+++ src/treeBranchTrunk.java
@@ -0,0 +1,8 @@
+public class treeBranchTrunk
+{
+    public static void main (String[] args)
+    {
+        System.out.println("The trees are in bloom");
+    }//end main method
+
+}//end driver class
